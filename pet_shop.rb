2ca2807e228ce7033d6c6c959bc1a0908f288e1c
Michael Robertson <michael_robertson_92@hotmail.co.uk>
2017-05-19 11:50:48 +0100

    first 5 tests commited

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,16 @@
 def pet_shop_name(pet_shop)
   return pet_shop[:name]
 end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, num)
+  pet_shop[:admin][:total_cash] += num
+  return total_cash(pet_shop)
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
